3bc83f6ef162542c2c61e536f97e4401f57d4167
Clément Yvernes <101457597+Cyvernes@users.noreply.github.com>
2023-07-25 13:34:49 -0400

    Update first_seeds_selector.py
    Add aGivenAmountOfSESeeds

--- first_seeds_selector.py
+++ first_seeds_selector.py
@@ -74,6 +74,22 @@ def aGivenAmountOfForegroundSESeeds(learner : ActiveLearningSAM) -> List[Tuple[i
     first_seeds = [
         s for s in learner.SE_Seeds if getValueinArrFromInputFormat(learner.GT_mask, s)
     ]
-    first_seeds = first_seeds[0 : min(len(first_seeds), amount)]
+    first_seeds = first_seeds[-min(len(first_seeds), amount):]
     learner.SE_Seeds = [s for s in learner.SE_Seeds if not s in first_seeds]
+    return (first_seeds, SE_mask)
+
+def aGivenAmountOfSESeeds(learner : ActiveLearningSAM) -> List[Tuple[int, int]]:
+    """This function encodes a strategy to find the first point to annotate.
+        A specific number of SE seeds is selected.
+        learner.SE_seeds is updated accordingly.
+    
+    :param learner: Learner
+    :type learner: ActiveLearningSAM
+    :return: List of first points to annotate.
+    :rtype: List[Tuple[int, int]]
+    """
+    amount = 4
+    SE_mask = learner.SE_masks.pop(-1)["segmentation"]
+    first_seeds = learner.SE_Seeds[-min(len(first_seeds), amount):]
+    learner.SE_Seeds = learner.SE_Seeds[0:- min(len(first_seeds), amount)]
     return (first_seeds, SE_mask)
